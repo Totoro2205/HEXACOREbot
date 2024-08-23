568de80f03e8e0c0e9ed9f0ab0ea566b928b308b
Alex <alexander.keepsolid@gmail.com>
2024-08-23 03:20:51 +0300

    staking

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -889,7 +889,7 @@ class Tapper:
                 logger.info(
                     f"<light-yellow>{self.session_name}</light-yellow> | "
                     f"Balance: <g>{balance:,} ({overall_tokens:,})</g> AGO | "
-                    f"Level: <g>{lvl}</g> | Place on leaderboard: <g>{rank}</g> | "
+                    f"Level: <g>{lvl}</g> | Rank: <g>{rank}</g> | "
                     f"Taps limit: <g>{max_taps:,}</g> | Tap size: <g>{tap_size}</g>"
                 )
 
